992b599ed34f6b05346bfa67938bcf1258358a16
Gabi012 <gkboves@gmail.com>
2026-07-14 11:08:41 -0300

    Adicionada estrutura para gerar arquivo

--- SistemaVendas.Worker/Services/RelatorioService.cs
+++ SistemaVendas.Worker/Services/RelatorioService.cs
@@ -9,62 +9,51 @@ public class RelatorioService
 {
 
     private readonly AppDbContext _context;
-
-
-
-    public RelatorioService(
-        AppDbContext context)
+    public RelatorioService(AppDbContext context)
     {
         _context = context;
     }
 
-
-
-
-
-    public async Task AtualizarStatusAsync(
-        Guid id,
-        string status)
+    public async Task AtualizarStatusAsync(Guid id,string status)
     {
 
-
-        var relatorio =
-            await _context.Relatorios
-            .FirstOrDefaultAsync(
-                x => x.Id == id);
-
-
+        var relatorio = await _context.Relatorios.FirstOrDefaultAsync(x => x.Id == id);
 
         if (relatorio == null)
         {
-
-            Console.WriteLine(
-                $"Relatório {id} não encontrado");
-
+            Console.WriteLine($"Relatório {id} não encontrado");
             return;
-
         }
-
-
 
         relatorio.Status = status;
 
-
-
         if (status == "Concluído")
         {
-            relatorio.DataConclusao =
-                DateTime.Now;
+            relatorio.DataConclusao = DateTime.Now;
         }
-
-
 
         await _context.SaveChangesAsync();
 
+        Console.WriteLine($"Relatório {id} => {status}");
+
+    }
+
+    public async Task FinalizarAsync(Guid id,string caminhoArquivo)
+    {
+
+        var relatorio = await _context.Relatorios.FirstOrDefaultAsync(x => x.Id == id);
+
+        if (relatorio == null)
+            return;
+
+        relatorio.Status = "Concluído";
+
+        relatorio.DataConclusao = DateTime.Now;
 
 
-        Console.WriteLine(
-            $"Relatório {id} => {status}");
+        relatorio.ArquivoGerado = caminhoArquivo;
+
+        await _context.SaveChangesAsync();
 
     }
 
