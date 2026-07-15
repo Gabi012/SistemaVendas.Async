60d18b62058ba994e66374355f2a1f861df21a69
Gabi012 <gkboves@gmail.com>
2026-07-15 15:13:41 -0300

    Adicionado contador de notificação

--- SistemaVendas.Web/Program.cs
+++ SistemaVendas.Web/Program.cs
@@ -55,6 +55,5 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
-app.MapHub<NotificacaoHub>("/notificacoes");
-
+app.MapHub<NotificacaoHub>("/hubs/notificacoes");
 app.Run();
